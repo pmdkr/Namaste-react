a1851e1e1b512b72d8effe1022ac3d08fc75a0ad
pmdkr <pramodkrlohra@gmail.com>
2023-10-14 19:07:55 +0530

    nested div added

--- App.js
+++ App.js
@@ -1,5 +1,12 @@
-const parant = React.createElement("div",{id : "parant"},
-React.createElement("div",{id:"child"},"I am a child")
+const parant = React.createElement("div", { id: "parant" }, [
+    React.createElement("div", { id: "child 1" }, [
+        React.createElement("h1", { id: "children 01" }, "I am from h1 tag"),
+        React.createElement("h2", { id: "children02" }, "Namaste react 🚀")]), 
+    
+    
+    React.createElement("div", { id: "child 2" }, 
+    [React.createElement("h1", { id: "children3" }, "I am also h1 tag"),
+    React.createElement("h2", { id: "children4" }, "I am from div 2")])]
 
 )
 console.log(parant);
